4b5a47f974ab015885bc0d8db6810764ee6024a9
Carlos <cbcarlos08@gmail.com>
2018-08-08 13:09:38 -0400

    Seção 5 Aula 50 Implementando o compondente de avaliações - pipes async e date

--- src/app/restaurants/restaurant/restaurants.service.ts
+++ src/app/restaurants/restaurant/restaurants.service.ts
@@ -21,4 +21,11 @@ export class RestaurantsService{
             .map(response => response.json())
             .catch(ErrorHandler.handleError)
     }
+
+    reviewOfRestaurant(id:string): Observable<any>{
+
+        return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError)
+    }
 }
