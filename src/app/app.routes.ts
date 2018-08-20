7273198aa5cf50c7c51ea029c3bb8e4ac003cf81
Carlos <cbcarlos08@gmail.com>
2018-08-20 12:15:58 -0400

    Seção 6 Aula 60 Aplicando feedback visual no formulário de compras

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import {RestaurantDetailComponent} from "./restaurant-detail/restaurant-detail.c
 import {MenuComponent} from "./restaurant-detail/menu/menu.component";
 import {ReviewsComponent} from "./restaurant-detail/reviews/reviews.component";
 import {MenuItemComponent} from "./restaurant-detail/menu-item/menu-item.component";
+import {OrderComponent} from "./order/order.component";
 
 export const ROUTES: Routes = [
     {path: '', component: HomeComponent},
@@ -16,5 +17,6 @@ export const ROUTES: Routes = [
         {path: 'menu', component: MenuComponent},
         {path: 'reviews', component: ReviewsComponent}
     ]},
+    {path: 'order', component: OrderComponent},
     {path: 'about', component: AboutComponent},
 ]
