03a81fdb7940f69ef13d96c89083f64f29c1c84b
Carlos <cbcarlos08@gmail.com>
2018-08-28 11:55:34 -0400

    Seção 7 Aula 73 Validação com reactive forms

--- src/app/order/order-form-group/order-form-group.component.ts
+++ src/app/order/order-form-group/order-form-group.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {RadioOption} from "../../shared/radio/radio-option.model";
 import {OrderService} from "../order.service";
 import {Router} from "@angular/router";
@@ -12,6 +12,8 @@ import {Order, OrderItem} from "../order.model";
   styles: []
 })
 export class OrderFormGroupComponent implements OnInit {
+  emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+  numberPattern = /^[0-9]*$/
   orderForm: FormGroup
   delivery = 8
 
@@ -26,13 +28,13 @@ export class OrderFormGroupComponent implements OnInit {
 
   ngOnInit() {
     this.orderForm = this.formBuilder.group({
-      name: this.formBuilder.control(''),
-      email: this.formBuilder.control(''),
-      emailConfirmation: this.formBuilder.control(''),
-      address: this.formBuilder.control(''),
-      number: this.formBuilder.control(''),
+      name: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      email: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      emailConfirmation: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      address: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
       optionalAddress: this.formBuilder.control(''),
-      paymentOption: this.formBuilder.control('')
+      paymentOption: this.formBuilder.control('', [Validators.required])
     })
   }
   itemsValue(): number{
