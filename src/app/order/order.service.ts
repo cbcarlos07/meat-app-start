40d26ffd7fcf2c39661fce1feed0278abc056c39
Carlos <cbcarlos08@gmail.com>
2018-08-31 08:00:14 -0400

    Seção 13 Aula 108 Refactoring da Aplicação

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -4,14 +4,15 @@ import {CartItem} from '../restaurant-detail/shopping-cart/cart-item.model';
 import {Order} from './order.model';
 import {Observable} from 'rxjs';
 import 'rxjs/add/operator/map'
-import {Http, Headers, RequestOptions} from '@angular/http';
+
 import {MEAT_API} from '../app.api';
+import {HttpClient} from "@angular/common/http";
 
 
 @Injectable()
 export class OrderService {
   constructor(private cartService: ShoppingCartService,
-              private http: Http) {}
+              private http: HttpClient) {}
   cartItems(): CartItem[] {
     return this.cartService.items
   }
@@ -33,12 +34,8 @@ export class OrderService {
     this.cartService.clear()
   }
   checkOrder(order: Order): Observable<string> {
-    const headers = new Headers()
-    headers.append('Content-type', 'application/json')
-    return this.http.post(`${MEAT_API}/orders`,
-                                JSON.stringify(order),
-                                new RequestOptions({headers: headers}))
-                      .map(response => response.json())
+
+    return this.http.post<Order>(`${MEAT_API}/orders`, order)
                       .map(order => order.id)
   }
 }
