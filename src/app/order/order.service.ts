faecaccbf6886c70772da4234e828fdd69e9c85f
Carlos <cbcarlos08@gmail.com>
2018-09-04 12:03:51 -0400

    Seção 15 Aula 119 Realizando uma compra com autenticação

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -6,13 +6,15 @@ import {Observable} from 'rxjs';
 import 'rxjs/add/operator/map'
 
 import {MEAT_API} from '../app.api';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {LoginService} from "../security/login/login.service";
 
 
 @Injectable()
 export class OrderService {
   constructor(private cartService: ShoppingCartService,
-              private http: HttpClient) {}
+              private http: HttpClient,
+              private loginService: LoginService) {}
   cartItems(): CartItem[] {
     return this.cartService.items
   }
@@ -34,8 +36,11 @@ export class OrderService {
     this.cartService.clear()
   }
   checkOrder(order: Order): Observable<string> {
-
-    return this.http.post<Order>(`${MEAT_API}/orders`, order)
+    let headers = new HttpHeaders()
+    if(this.loginService.isLoggedIn()){
+      headers = headers.set('Authorization', `Bearer ${this.loginService.user.accessToken}`)
+    }
+    return this.http.post<Order>(`${MEAT_API}/orders`, order, {headers: headers})
                       .map(order => order.id)
   }
 }
